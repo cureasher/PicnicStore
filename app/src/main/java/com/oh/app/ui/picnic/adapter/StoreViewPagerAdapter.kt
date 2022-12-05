284e33ccaa9123c39f2d37aeac18b3a2c825315d
cureasher <cureasher@gmail.com>
2022-12-05 18:50:49 +0900

    feat: Create ViewPager and BottomSheet Text Update

---
+++ app/src/main/java/com/oh/app/ui/picnic/adapter/StoreViewPagerAdapter.kt
@@ -0,0 +1,34 @@
+package com.oh.app.ui.picnic.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.oh.app.R
+import com.oh.app.data.store.Row
+
+class StoreViewPagerAdapter(idolList: List<Row>) :
+    RecyclerView.Adapter<StoreViewPagerAdapter.PagerViewHolder>() {
+    var item = idolList
+
+    override fun getItemCount(): Int = item.size
+    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        holder.store.text = item[position].UPSO_NM
+        holder.menu.text = item[position].BIZCND_CODE_NM
+        holder.address.text = item[position].RDN_CODE_NM
+        Log.d("로그", "onBindViewHolder: ${item[position]}")
+    }
+
+    inner class PagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val store = itemView.findViewById<TextView>(R.id.storeTextView)
+        val menu = itemView.findViewById<TextView>(R.id.menuTextView)
+        val address = itemView.findViewById<TextView>(R.id.addressTextview)
+    }
+
+    override fun onCreateViewHolder(view: ViewGroup, viewType: Int): PagerViewHolder {
+        val view = LayoutInflater.from(view.context).inflate(R.layout.item_store, view, false)
+        return PagerViewHolder(view)
+    }
+}
